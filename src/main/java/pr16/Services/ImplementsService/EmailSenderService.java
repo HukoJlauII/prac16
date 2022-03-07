04bdf5216b1d59281042fdb60ff240f7c15486cf
Nekit <polosatay_zebra@mail.ru>
2022-03-07 23:06:01 +0300

    Added AOP structure and EmailSender for all methods

---
+++ src/main/java/pr16/Services/ImplementsService/EmailSenderService.java
@@ -0,0 +1,25 @@
+package pr16.Services.ImplementsService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.stereotype.Service;
+
+@EnableAsync
+@Service
+public class EmailSenderService implements pr16.Services.EmailSenderService {
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    @Override
+    public void sendMail(String text) {
+
+        SimpleMailMessage mailMessage=new SimpleMailMessage();
+        mailMessage.setTo("nikpuk637@gmail.com");
+        mailMessage.setText(text);
+        mailMessage.setSubject("Test message");
+        mailSender.send(mailMessage);
+    }
+}
