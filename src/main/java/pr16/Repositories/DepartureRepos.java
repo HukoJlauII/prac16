a7425dc726ee9b7d0b5ac1960d13292ba95079f4
Nekit <polosatay_zebra@mail.ru>
2022-03-11 01:19:02 +0300

    Added saving entities mechanism

--- src/main/java/pr16/Repositories/DepartureRepos.java
+++ src/main/java/pr16/Repositories/DepartureRepos.java
@@ -1,14 +1,17 @@
 package pr16.Repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import pr16.Entity.Departure;
-import pr16.Entity.PostOffice;
-
-import java.util.List;
 
 @Repository
 public interface DepartureRepos extends JpaRepository<Departure,Integer> {
     Departure findDepartureByType(String type);
-//    List<Departure> findAllByPostOffice(PostOffice postOffice);
+    @Modifying
+    @Query(value = "COPY postgres.jpa.departure TO 'C:\\temp\\Entities\\departure.csv'" +
+            "delimiter ',' " +
+            "csv header encoding 'UTF-8'", nativeQuery = true)
+    void saveDeparture();
 }
